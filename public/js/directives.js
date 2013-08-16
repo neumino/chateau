fbc59c762bfc7e64e189ef4a103ae70e22f3e00f
Michel <orphee@gmail.com>
2013-08-16 16:52:41 -0700

    Fix auto expand textarea

--- public/js/directives.js
+++ public/js/directives.js
@@ -8,6 +8,8 @@ angular.module('chateau.directives', [])
             link: function( $scope, elem, attrs) {
                 elem.bind('keyup', function($event) {
                     var element = $event.target;
+                    var scrollPosition = $(window).scrollTop()
+                    $(element).height(0);
                     var height = $(element)[0].scrollHeight
 
                     // 8 is for the padding
@@ -15,6 +17,7 @@ angular.module('chateau.directives', [])
                         height = 28
                     }
                     $(element).height(height-8)
+                    $(window).scrollTop(scrollPosition)
                 });
                 setTimeout( function() {
                     var element = elem;
